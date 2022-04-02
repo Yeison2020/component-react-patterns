65cd2514930f83b9ef4ab80760201744b5470c11
Yeison <enmanuelquiterios@gmail.com>
2022-04-02 12:13:58 -0400

    Implemented Custom hook

--- src/02-component-patterns/hooks/useProducts.ts
+++ src/02-component-patterns/hooks/useProducts.ts
@@ -4,7 +4,7 @@ export const useProducts = () => {
   const [counter, setCounter] = useState(0);
   const increaseBy = (value: number) => {
     // Here the Math.max If the Values goes down zero the Math.max will return my counter to be zero meaning that the counter will never go under zero.
-    setCounter((prev) => Math.max(prev + 1, 0));
+    setCounter((prev) => Math.max(prev + value, 0));
   };
   return { counter, increaseBy };
 };

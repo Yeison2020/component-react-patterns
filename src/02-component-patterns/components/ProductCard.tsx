4811f81bff2fcb52f3a545e716851a758e0b8242
Yeison <enmanuelquiterios@gmail.com>
2022-04-02 11:48:09 -0400

    Adding components

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,11 +1,23 @@
 import styles from "../styles/styles.module.css";
+import noImage from "../assets/no-image.jpg";
 
 const ProductCard = () => {
   console.log(styles);
   return (
     <div className={styles.productCard}>
       {/* Because I put the Image inside of the public foler I was abale to fecth just like this ==>  */}
-      <img src="./coffee-mug.png" alt="coffee mug" />
+      <img
+        className={styles.productImg}
+        src="./coffee-mug.png"
+        alt="coffee Mug"
+      />
+      {/* <img className={styles.productImg} src={noImage} alt="coffee Mug" /> */}
+      <span className={styles.productDescription}>Coffee Mug</span>
+      <div className={styles.buttonsContainer}>
+        <button className={styles.buttonMinus}>-</button>
+        <div className={styles.countLabel}> 0 </div>
+        <button className={styles.buttonAdd}>+</button>
+      </div>
     </div>
   );
 };

4947a568fef3a7424f1ad4e268edc49ae9522a53
Yeison <enmanuelquiterios@gmail.com>
2022-04-02 11:43:02 -0400

    Adding nested patterns

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -3,8 +3,9 @@ import styles from "../styles/styles.module.css";
 const ProductCard = () => {
   console.log(styles);
   return (
-    <div>
-      <h2>Product Card</h2>
+    <div className={styles.productCard}>
+      {/* Because I put the Image inside of the public foler I was abale to fecth just like this ==>  */}
+      <img src="./coffee-mug.png" alt="coffee mug" />
     </div>
   );
 };

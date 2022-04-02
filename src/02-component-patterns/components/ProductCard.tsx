affa205c9d8c4e996d7d28428e27fc6d55646801
Yeison <enmanuelquiterios@gmail.com>
2022-04-02 11:59:58 -0400

    Creating patterns

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,7 +1,17 @@
 import styles from "../styles/styles.module.css";
 import noImage from "../assets/no-image.jpg";
 
+import { useState } from "react";
+
 const ProductCard = () => {
+  const [counter, setCounter] = useState(0);
+
+  // Handler Functions
+  const increaseBy = (value: number) => {
+    // Here the Math.max If the Values goes down zero the Math.max will return my counter to be zero meaning that the counter will never go under zero.
+    setCounter((prev) => Math.max(prev + value, 0));
+  };
+
   console.log(styles);
   return (
     <div className={styles.productCard}>
@@ -14,9 +24,13 @@ const ProductCard = () => {
       {/* <img className={styles.productImg} src={noImage} alt="coffee Mug" /> */}
       <span className={styles.productDescription}>Coffee Mug</span>
       <div className={styles.buttonsContainer}>
-        <button className={styles.buttonMinus}>-</button>
-        <div className={styles.countLabel}> 0 </div>
-        <button className={styles.buttonAdd}>+</button>
+        <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
+          -
+        </button>
+        <div className={styles.countLabel}> {counter} </div>
+        <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}>
+          +
+        </button>
       </div>
     </div>
   );

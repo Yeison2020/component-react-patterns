84f15e51eeab1f52c0354223cbb63030bb73dfa5
Yeison <enmanuelquiterios@gmail.com>
2022-04-02 11:33:09 -0400

    Adding styles

---
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import styles from "../styles/styles.module.css";
+
+const ProductCard = () => {
+  console.log(styles);
+  return (
+    <div>
+      <h2>Product Card</h2>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductCard from "../components/ProductCard";
+
+const ShoppingPage = () => {
+  return (
+    <div>
+      <h1>Shopping Store</h1>
+      <hr />
+      <ProductCard />
+    </div>
+  );
+};
+
+export default ShoppingPage;

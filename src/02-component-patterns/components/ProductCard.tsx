1d7d5c77126abfde776a606767405a9438915b5b
Yeison <enmanuelquiterios@gmail.com>
2022-04-05 10:14:35 -0400

    Updates

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,14 +1,17 @@
 import styles from "../styles/styles.module.css";
 import { useProducts } from "../hooks/useProducts";
 import { createContext } from "react";
-import { ProductContextProps, Props } from "../interfaces/interfaces";
+import {
+  ProductContextProps,
+  ProductCardProps,
+} from "../interfaces/interfaces";
 import { ProductImage } from "./ProductImage";
 import { ProductTitle } from "./ProductTitle";
 import { ProductButtons } from "./ProductButtons ";
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 // Main Component with integrated Components
-const ProductCard = ({ children, product }: Props) => {
+const ProductCard = ({ children, product }: ProductCardProps) => {
   // Customs Hooks Imports
   const { counter, increaseBy } = useProducts();
   return (

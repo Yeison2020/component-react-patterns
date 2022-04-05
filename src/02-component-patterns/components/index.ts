dde8d890d93fdcee7dd7f84467870ec45bf9f83e
Yeison <enmanuelquiterios@gmail.com>
2022-04-05 10:47:23 -0400

    Added Comments

--- src/02-component-patterns/components/index.ts
+++ src/02-component-patterns/components/index.ts
@@ -4,3 +4,5 @@ export { ProductTitle } from "./ProductTitle";
 export { ProductCard } from "./ProductCard";
 
 // The other branch Contains another way to extend my Components as Object to Thier parent Component
+
+// Check those patterns and Review how they are connected and how theur work

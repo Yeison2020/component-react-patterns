6f44d138c28bd9ccff6c6f2baad24c3bdda5c63d
Yeison <enmanuelquiterios@gmail.com>
2022-04-05 10:10:22 -0400

    Refactorization done

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,22 +1,16 @@
 import styles from "../styles/styles.module.css";
 import { useProducts } from "../hooks/useProducts";
-import { createContext, useContext } from "react";
+import { createContext } from "react";
 import { ProductContextProps, Props } from "../interfaces/interfaces";
 import { ProductImage } from "./ProductImage";
 import { ProductTitle } from "./ProductTitle";
 import { ProductButtons } from "./ProductButtons ";
-
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
-
 // Main Component with integrated Components
-
 const ProductCard = ({ children, product }: Props) => {
   // Customs Hooks Imports
   const { counter, increaseBy } = useProducts();
-
-  //
-  // console.log(styles);
   return (
     <Provider
       value={{

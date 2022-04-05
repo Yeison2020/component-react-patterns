cb2f37bdb9a3c8c192950361693b9a218e112309
Yeison <enmanuelquiterios@gmail.com>
2022-04-05 10:42:15 -0400

    Adding Notes Comment in Component Index

--- src/02-component-patterns/components/index.ts
+++ src/02-component-patterns/components/index.ts
@@ -2,3 +2,5 @@ export { ProductButtons } from "./ProductButtons ";
 export { ProductImage } from "./ProductImage";
 export { ProductTitle } from "./ProductTitle";
 export { ProductCard } from "./ProductCard";
+
+// The other branch Contains another way to extend my Components as Object to Thier parent Component
